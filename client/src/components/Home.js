798fa2a790cfa6d1c641baa2c5553edae1273f6a
LerichO <Lerich.Osay@UTDallas.edu>
2024-11-11 14:50:47 -0600

    client tweaks

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,39 +9,17 @@ const Home = () => {
   // example posts to test state with, this will be replaced in time with a more dynamic solution
   const [posts, setPosts] = useState([
     {
-      id: '1',
-      title: 'First Post',
-      description: 'This is the description of the first post.',
-      likes: 5,
-      comments: [
-        'Great post!',
-        'Thanks for sharing!',
-        'Very informative!'
-      ]
-    },
-    {
-      id: '2',
-      title: 'Second Post',
-      description: 'This is the description of the second post.',
-      likes: 10,
-      comments: [
-        'I learned a lot!',
-        'Interesting read!',
-        'Can you elaborate more?'
-      ]
-    },
-    {
-      id: '3',
-      title: 'Third Post',
-      description: 'This is the description of the third post.',
-      likes: 2,
-      comments: [
-        'Nice perspective!',
-        'I disagree with some points.',
-        'Looking forward to your next post!'
-      ]
+      id: '',
+      title: '',
+      description: '',
+      likes: 0
     }
   ]);
+
+  // useEffect hook used to execute code on render of the component
+  // will implement this in level-3
+  useEffect(() => {}, [])
+
 
   // return for this function returns the JSX of the React component
   return (
